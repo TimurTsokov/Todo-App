b83d9099ee6f27a2f2288a70570944411491345c
TimurTsokov <tsokov2t1m5@gmail.com>
2018-11-27 22:36:16 +0200

    commit filter

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ export default class App extends Component {
             this.createTodoItem('Kill people'),
             this.createTodoItem('Have a nice day')
         ],
-        term: ''
+        term: '',
+        filter: 'all'
     };
 
     createTodoItem(label) {
@@ -94,7 +95,9 @@ export default class App extends Component {
     onSearchChan = (term) => {
         this.setState({term});
     };
-
+    onFilterChng = (filter) => {
+        this.setState({filter});
+    };
     search(items, term) {
         if (term.length === 0) {
             return items;
@@ -106,10 +109,23 @@ export default class App extends Component {
         })
     }
 
-    render() {
-        const {todoData, term} = this.state;
+    filter(items, filter) {
+        switch (filter) {
+            case 'all':
+                return items;
+            case 'active':
+                return items.filter((item) => !item.done);
+            case 'done' :
+                return items.filter((item) => item.done);
+            default:
+                return items;
+        }
+    }
 
-        const visibleItems = this.search(todoData, term);
+    render() {
+        const {todoData, term, filter} = this.state;
+
+        const visibleItems = this.filter(this.search(todoData, term), filter);
 
         const doneCount = todoData.filter((el) => el.done).length;
         const todoCount = todoData.length - doneCount;
@@ -120,7 +136,10 @@ export default class App extends Component {
                     <SearchPanel
                         onSearchChange={this.onSearchChan}
                     />
-                    <ItemStatusFilter/>
+                    <ItemStatusFilter
+                        filter={filter}
+                        onFilterChange={this.onFilterChng}
+                    />
                 </div>
                 <TodoList
                     todos={visibleItems}
--- src/Components/item-status-filter.js
+++ src/Components/item-status-filter.js
@@ -2,12 +2,33 @@ import React, {Component} from 'react';
 import './sass/item-status-filter.sass'
 
 export default class ItemStatusFilter extends Component {
+
+    buttons = [
+        {name: 'all', label: 'All'},
+        {name: 'active', label: 'Active'},
+        {name: 'done', label: 'Done'}
+    ];
+
     render() {
+
+        const {filter, onFilterChange} = this.props;
+
+        const buttons = this.buttons.map(({name, label}) => {
+            const isActive = filter === name;
+            const classNm = isActive ? 'btn-info' : 'btn-outline-secondary';
+            return (
+                <button type="button"
+                        className={`btn ${classNm}`}
+                        key={name}
+                        onClick={() => onFilterChange(name)}>
+                    {label}
+                </button>
+            )
+        });
+
         return (
             <div className="btn-group">
-                <button className="btn btn-info" type="button">All</button>
-                <button className="btn btn-outline-second" type="button">Active</button>
-                <button className="btn btn-outline-second" type="button">Done</button>
+                {buttons}
             </div>
         )
     }
